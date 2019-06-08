2da446770d120aaab25b865ef3147b372b65d246
Triumph <archie200@icloud.com>
2019-06-08 09:28:16 -0500

    added questions

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+var trivia = [
+    {
+    question: "Why did the cat jump over the moon?",
+    answers: {a: 'This is impossible',b: 'The dog was chasing it',c: 'The cat is fully cross-fit'},
+    },
+    {
+        question: "What is the meaning of life?",
+        answers: {a: 'To learn',b: 'To experience',c: 'To feel'},
+    },
+    {
+    question: "Why do we live in Illinois?",
+    answers: {a: 'The cold winters',b: 'The high taxes',c: 'The crooked politicians'},
+    },
+];
